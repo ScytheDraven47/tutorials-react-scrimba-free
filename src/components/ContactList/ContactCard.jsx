ece157765eef16b33ce7348afd68d1dd6dddf5f2
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2021-11-28 12:57:26 +1300

    feat: ContactList

---
+++ src/components/ContactList/ContactCard.jsx
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+import { FaPhone, FaEnvelope } from 'react-icons/fa'
+
+const ContactCard = ({ id, imageUrl, name, phone, email }) => {
+	return (
+		<StyledDiv>
+			<img
+				className='contact__image'
+				src={`${imageUrl}&sig=${id}`}
+				alt={name}
+			/>
+			<h3 className='contact__name'>{name}</h3>
+			<a className='contact__info' href={`tel:${phone}`}>
+				<FaPhone />
+				<span>{phone}</span>
+			</a>
+			<a className='contact__info' href={`mailto:${email}`}>
+				<FaEnvelope />
+				<span>{email}</span>
+			</a>
+		</StyledDiv>
+	)
+}
+
+export default ContactCard
+
+const StyledDiv = styled.div`
+	border-radius: 10px;
+	padding: 1em;
+	background: white;
+	color: black;
+
+	.contact__image {
+		width: 100%;
+	}
+
+	.contact__info {
+		display: flex;
+		align-items: center;
+		gap: 0.5em;
+		color: black;
+		font-size: 0.8rem;
+		line-height: 1.5em;
+		text-decoration: none;
+		span {
+			color: gray;
+		}
+	}
+`
---
+++ src/components/ContactList/index.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+import ContactCard from './ContactCard'
+
+const ContactList = () => {
+	const contacts = [
+		{
+			name: 'Mr. Whiskerson',
+			imageUrl: 'https://source.unsplash.com/300x200/?cat',
+			phone: '(212) 555-1234',
+			email: 'mr.whiskaz@catnap.meow',
+		},
+		{
+			name: 'Fluffykins',
+			imageUrl: 'https://source.unsplash.com/300x200/?cat',
+			phone: '(212) 555-2345',
+			email: 'fluff@me.com',
+		},
+		{
+			name: 'Felix',
+			imageUrl: 'https://source.unsplash.com/300x200/?cat',
+			phone: '(212) 555-4567',
+			email: 'thecat@hotmail.com',
+		},
+		{
+			name: 'Pumpkin',
+			imageUrl: 'https://source.unsplash.com/300x200/?cat',
+			phone: '(0800) CAT KING',
+			email: 'pumpkin@scrimba.com',
+		},
+	]
+
+	return (
+		<StyledDiv>
+			{contacts.map((c, index) => (
+				<ContactCard key={index} id={index} {...c} />
+			))}
+		</StyledDiv>
+	)
+}
+
+export default ContactList
+
+const StyledDiv = styled.div`
+	width: 100%;
+	display: flex;
+	flex-wrap: wrap;
+	gap: 2em;
+	padding: 2rem;
+
+	& > * {
+		width: calc(50% - 1rem);
+	}
+`
