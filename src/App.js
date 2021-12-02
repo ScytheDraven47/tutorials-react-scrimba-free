0faecb88b110b6fa1a071958016f7e91d72bbdb0
ScytheDraven47 <ben.rogers.mckee@gmail.com>
2021-12-02 18:18:43 +1300

    refactor: App disable lint warning

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
 import styled from 'styled-components'
+
+/* eslint-disable no-unused-vars */
 import Card from './components/Card'
 import LightBoxes from './components/LightBoxes'
 import MemeGenerator from './components/MemeGenerator'
-// import TravelJournal from './components/TravelJournal'
-// import ReactFacts from './components/ReactFacts'
-// import DigitalProfile from './components/DigitalProfile'
-// import AirBnb from './components/AirBnb'
-// import ContactList from './components/ContactList'
-// import Jokes from './components/Jokes'
+import TravelJournal from './components/TravelJournal'
+import ReactFacts from './components/ReactFacts'
+import DigitalProfile from './components/DigitalProfile'
+import AirBnb from './components/AirBnb'
+import ContactList from './components/ContactList'
+import Jokes from './components/Jokes'
 
 const App = () => (
 	<AppWrapper>
-		<Card width='35rem'>
-			<LightBoxes />
-		</Card>
-		<Card width='550px'>
-			<MemeGenerator />
-		</Card>
 		{/* <Card>
 			<ReactFacts />
-		</Card>
-		<Card>
+		</Card> */}
+		{/* <Card>
 			<DigitalProfile />
-		</Card>
-		<Card width='30rem'>
+		</Card> */}
+		{/* <Card width='30rem'>
 			<AirBnb />
-		</Card>
-		<Card width='35rem'>
+		</Card> */}
+		{/* <Card width='35rem'>
 			<ContactList />
-		</Card>
+		</Card> */}
 		<Card>
 			<Jokes />
 		</Card>
-		<Card>
+		{/* <Card>
 			<TravelJournal />
+		</Card> */}
+		{/* <Card width='550px'>
+			<MemeGenerator />
+		</Card> */}
+		{/* <Card width='35rem'>
+			<LightBoxes />
 		</Card> */}
 	</AppWrapper>
 )
